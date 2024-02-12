ab3c776d800b86b7e16750678ddec45e2b82843c
Emile <emile@shodiev.ru>
2024-02-12 14:59:27 +0400

    gpt describe experiments

--- src/config.js
+++ src/config.js
@@ -5,10 +5,11 @@ exports.config = {
   gpt: {
     enabled: true,
     allowedChats: [-1001278382802, -1001799649218],
-    historyLength: 200,
+    historyLength: 500,
     completionParams: {
       // https://platform.openai.com/docs/api-reference/chat/create
-      model: "gpt-3.5-turbo-0125",
+      model: 'gpt-4',
+      // model: "gpt-3.5-turbo-0125",
       temperature: 1,
       top_p: 1 
     }
